bcf5f3755ffe255fc0b2884c10d1af28e3796eab
muhammadbilalali799 <bsef17m043@pucit.edu.pk>
2020-07-26 17:51:20 +0500

    Revised

--- counter/app/src/main/java/com/example/counter/MainActivity.java
+++ counter/app/src/main/java/com/example/counter/MainActivity.java
@@ -10,16 +10,17 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView tv;
-    Button btnInc,btnReset;
+    Button btnInc,btnReset,btnDec;
     int c=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv =  findViewById(R.id.count);
-        btnInc= findViewById(R.id.increment);
-        btnReset= findViewById(R.id.reset);
+        tv =  findViewById(R.id.conuterText);
+        btnInc= findViewById(R.id.incBtn);
+        btnDec= findViewById(R.id.decBtn);
+        btnReset= findViewById(R.id.resetBtn);
 
         btnInc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +36,13 @@ public class MainActivity extends AppCompatActivity {
                 tv.setText(""+c);
             }
         });
+        btnDec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                c--;
+                tv.setText(""+c);
+            }
+        });
+
     }
 }
